47d13544106087464ec48e03ca3f06df23762500
richard ybanez <j.richardybanez7@gmail.com>
2023-01-10 16:22:41 +0800

    removed comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import { Component } from 'react';
 import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component.jsx';
